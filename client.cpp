6df46bff777e151867f8fd4b1ea0eca12a82a4bd
thatdoctorperson <nkell1@ocdsb>
2015-10-11 20:09:38 -0400

    Code update

--- client.cpp
+++ client.cpp
@@ -1,75 +1,116 @@
-#include <winsock.h>
-#include <string.h>
+/*************************************************************************************
+ *                                                                                   *
+ *                            RovOS Windows Client Code                              *
+ *                                       c++                                         *
+ *                                      Nova                                         *
+ *                                                                                   *
+ *************************************************************************************/
+
+#include <cstring>
 #include <iostream>
 using namespace std;
+//windows server header file
+#include <winsock.h>
 
+//weird thing required on windows
 #pragma comment(lib,"ws2_32.lib")
 
+//port number socket server is running off of
 #define PORT 42
 
-void blankOut(char string[]);
+//empties a character array
+void blankOut(char string[]){
+	for(int i = 0; i <= 100; i++){
+		string[i] = ' ';
+	}
+}
 
 int main(){
+	//weird stuff windows requires
 	WSADATA wsaData;
 	WSAStartup(0x0202, &wsaData);
 	
-	int thisSocket;
-	struct sockaddr_in destination;
+	//declaring socket variable and srvrAddr structure
+	int srvrSocket;
+	struct sockaddr_in srvrAddr;
 
-	destination.sin_family = AF_INET;
-	thisSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
-	if (thisSocket < 0)
+	//creating socket
+	srvrAddr.sin_family = AF_INET;
+	srvrSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+	if (srvrSocket < 0)
 	{
 		cout << "Socket Creation FAILED!" << endl;
-		return 0;
+		return 1;
 	}
 	cout << "Socket Created!" << endl;
 	
-	destination.sin_port = htons (PORT);
-	destination.sin_addr.s_addr = inet_addr("192.168.1.150");
-	if (connect(thisSocket,(struct sockaddr *)&destination,sizeof(destination))!=0){
+	//connecting to socket
+	srvrAddr.sin_port = htons (PORT);
+	srvrAddr.sin_addr.s_addr = inet_addr("192.168.1.150");
+	if (connect(srvrSocket,(struct sockaddr *)&srvrAddr,sizeof(srvrAddr))!=0){
 		cout << "No such Host!" << endl;
-		if (thisSocket) closesocket(thisSocket);
-		return 0;
+		if (srvrSocket) closesocket(srvrSocket);
+		return 1;
 	}
 	cout << "CONNECTED!" << endl;
 	
+	//declaring variables for sending commands
 	char again = 'y';
 	char buffer[101] = {0};
 	int n;
 	
+	//sends commands to linux server
 	while(again == 'y'){
+		
+		//clearing buffer
 		blankOut(buffer);
+		
+		//getting commands from user
 		cout << "Enter command: ";
 		cin.get(buffer, 25);
 		cin.ignore();
-		n = send(thisSocket, buffer, strlen(buffer), 0);
+		
+		//sending command
+		n = send(srvrSocket, buffer, strlen(buffer), 0);
 		if(n < 0){
 			cout << "Sending to Socket FAILED!" << endl;
-			if (thisSocket) closesocket(thisSocket);
-			return 0;
+			if (srvrSocket) closesocket(srvrSocket);
+			return 1;
 		}
 		cout << "Message Sent!" << endl;
 		
+		//clearing buffer
+		blankOut(buffer);
+		
+		//receving server reply
+		n = recv(srvrSocket, buffer, 100, 0);
+		if(n < 0){
+			cout << "Reading from Socket FAILED!" << endl;
+			if (srvrSocket) closesocket(srvrSocket);
+			return 1;
+		}
+		//re,oving extra characters from server message;
+		for(int i = 0; i < n; i++){
+			cout << buffer[i];
+		}
+		cout << endl;
+		
+		//loops for multiple commands
 		cout << "Would you like to send another command (y/n): ";
 		cin >> again;
 		cin.ignore();
 	}
 	
+	//telling server to shutdown
 	char end[4] = "end";
-	n = send(thisSocket, end, 4, 0);
+	n = send(srvrSocket, end, 4, 0);
 	if(n < 0){
 		cout << "Sending to Socket FAILED!" << endl;
-		if (thisSocket) closesocket(thisSocket);
-		return 0;
+		if (srvrSocket) closesocket(srvrSocket);
+		return 1;
 	}
 	
-	closesocket(thisSocket);
+	//closing socket and ending program
+	closesocket(srvrSocket);
 	return 0;
-}
-
-void blankOut(char string[]){
-	for(int i = 0; i <= 100; i++){
-		string[i] = ' ';
-	}
 }
--- server.cpp
+++ server.cpp
@@ -1,83 +1,111 @@
+/*************************************************************************************
+ *                                                                                   *
+ *                             RovOS Linux Server Code                               *
+ *                                        c                                          *
+ *                                      Nova                                         *
+ *                                                                                   *
+ *************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//linux server header files
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
+//port number socket server is running off of
 #define PORT 42
 
-void blankOut(char string[]);
+//empties a character array
+void blankOut(char string[]){
+	for(int i = 0; i <= 100; i++){
+		string[i] = ' ';
+	}
+}
 
 int main(){
 	
-	int thisSocket;
-	struct sockaddr_in destination;
-
-	destination.sin_family = AF_INET;
-	thisSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
-	if (thisSocket < 0)
+	//declaring socket variable and srvrAddr structure
+	int srvrSocket;
+	struct sockaddr_in srvrAddr, cliAddr;
+	socklen_t cliLen = sizeof(cliAddr);
+	
+	//creating socket
+	srvrAddr.sin_family = AF_INET;
+	srvrSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+	if (srvrSocket < 0)
 	{
 		printf("Socket Creation FAILED!\n");
 		return 0;
 	}
 	printf("Socket Created!\n");
 	
-	destination.sin_port = htons (PORT);
-	destination.sin_addr.s_addr = INADDR_ANY;
-	if (bind(thisSocket, (struct sockaddr *)&destination, sizeof(destination))<0){
+	//binding socket
+	srvrAddr.sin_port = htons (PORT);
+	srvrAddr.sin_addr.s_addr = INADDR_ANY;
+	if (bind(srvrSocket, (struct sockaddr *)&srvrAddr, sizeof(srvrAddr))<0){
 		printf("Binding Socket FAILED!\n");
-		if (thisSocket) close(thisSocket);
+		if (srvrSocket) close(srvrSocket);
 		return 0;
 	}
 	printf("Socket Bound!\n");
-
+	
+	//listening for connections
 	printf("Listening on %d...\n", PORT);
-	if (listen(thisSocket, 5)<0){
+	if (listen(srvrSocket, 5)<0){
 		printf("Listening on Socket FAILED!\n");
-		if (thisSocket) close(thisSocket);
+		if (srvrSocket) close(srvrSocket);
 		return 0;
 	}
 	
-	struct sockaddr_in clientAddress;
-	socklen_t clientSize = sizeof(clientAddress);
-	thisSocket= accept(thisSocket, (struct sockaddr *)&clientAddress, &clientSize);
-	if (thisSocket<0)
+	//accepting connection
+	srvrSocket = accept(srvrSocket, (struct sockaddr *)&cliAddr, &cliLen);
+	if (srvrSocket<0)
 	{
 		printf("Socket Connection FAILED!\n");
-		if (thisSocket) close(thisSocket);
+		if (srvrSocket) close(srvrSocket);
 		return 0;
 	}
 	printf("Connection Established!\n");
 	
-	
+	//receiving commands
 	char buffer[101];
+	int n;
 	while(strcmp(buffer,"end") != 0){
-		int n;
+		
+		//clearing buffer
 		blankOut(buffer);
-		n = read(thisSocket, buffer, 100);
+		
+		//reading command
+		n = read(srvrSocket, buffer, 100);
 		if(n < 0){
 			printf("Reading from Socket FAILED!\n");
-			if (thisSocket) close(thisSocket);
+			if (srvrSocket) close(srvrSocket);
 			return 0;
 		}
+		
+		//replies to client
+		n = write(srvrSocket, "Command Executing!", 18);
+		if(n < 0){
+			printf("Writing to Socket FAILED!\n");
+			if (srvrSocket) close(srvrSocket);
+			return 0;
+		}
+		
+		//runs commands
 		if(strcmp(buffer,"end") != 0){
 			printf("Command Received: %s\n", buffer);
 			system(buffer);
+		//shuts down server if no more commands are coming
 		}else{
 			printf("Closing Program...\n");
 		}
 	}
 	
-	close(thisSocket);
-	
+	//closing socket and ending program
+	close(srvrSocket);
 	return 0;
-}
-
-void blankOut(char string[]){
-	for(int i = 0; i <= 100; i++){
-		string[i] = ' ';
-	}
 }
